c034999bb1adbcc1c5b3bfce68afc45851440f77
girishkumarkh <girishkumarkh@gmail.com>
2015-06-28 20:18:10 +0100

    urls

--- jackson/urls.py
+++ jackson/urls.py
@@ -1,10 +1,16 @@
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 urlpatterns = [
     # Examples:
     # url(r'^$', 'jackson.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^homepage', 'lyrics.views.homepage', name='homepage'),
     url(r'^admin/', include(admin.site.urls)),
 ]
+
+
+if settings.DEBUG:
+	urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
